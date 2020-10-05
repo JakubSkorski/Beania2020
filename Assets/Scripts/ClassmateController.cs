515edd77971c888a1aba665aa35073de7c078c31
Radosław Kmiotek <rk58690poczta@outlook.com>
2020-10-05 20:21:26 +0200

    Add a controller for NPC - ClassmateController

---
+++ Assets/Scripts/ClassmateController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ClassmateController : MonoBehaviour
+    {
+        //public float movmentSpeed;
+        public string displayName;
+        public int id;
+
+        private Classmate me;
+        private Animator anim;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+            me = new Classmate() { DisplayName = displayName, Id = id };
+
+            var x = gameObject.GetComponentInChildren<TextMesh>();
+            x.text = me.DisplayName;
+        }
+
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+           var player = collision.gameObject.GetComponent<PlayerController>();
+           if (player != null)
+           {
+                player.Found(me);
+           }
+       }
+
+        void OnCollisionEnter2D(Collision2D col)
+        {
+            Debug.Log("Collision");
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            //var player = collision.gameObject.GetComponent<PlayerController>();
+            //if (player != null)
+            //{
+            //    player.ExitTarget();
+            //}
+        }
+
+        // Update is called once per frame
+        /*void Update()
+        {
+          
+
+        }*/
+    }
+}
