11c0e15ad8ea10069da226a71e38179a857ff8a7
Radosław Kmiotek <rk58690poczta@outlook.com>
2020-10-07 15:18:46 +0200

    Add door and stairs controllers.

---
+++ Assets/Scripts/StairsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    // SCHODY
+    // gdy gracz wejdzie na schody zmieniany jest model szkoły z parteru na 2 pietro.
+    public class StairsController
+    {
+        private int floorNumber = 0;
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            floorNumber = (floorNumber == 1) ? 0 : 1;
+
+            // change floor object
+        }
+    }
+}
