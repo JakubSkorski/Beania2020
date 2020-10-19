8aefab414c1905101b797868b1d801caa4a9e5d8
Garneek <50029133+Garneek@users.noreply.github.com>
2020-10-19 18:36:52 +0200

    Przed wyruszeniem w droge nalerzy zebrac druzyne

--- Assets/Scripts/BG2PortalControllerForPlayer.cs
+++ Assets/Scripts/BG2PortalControllerForPlayer.cs
@@ -10,19 +10,37 @@ public class BG2PortalControllerForPlayer : MonoBehaviour
 
     public GameObject prop_clone_GO;
 
+    public AudioSource audioData;
+
+    void Start() {
+        audioData = GetComponent<AudioSource>();
+    }
+    private bool played = false;
+
 
     IEnumerator OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player")&&!played)
         {
+            played = true;
+            audioData.Play(0);
+            yield return new WaitForSeconds(4);
+
+            GameObject portal1 = (GameObject)Instantiate(PortalSprite, new Vector3(collision.gameObject.transform.position.x, collision.gameObject.transform.position.y, collision.gameObject.transform.position.z), Quaternion.identity);
             GameObject portal = (GameObject)Instantiate(PortalSprite, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
 
+            audioData.Pause();
+
             yield return new WaitForSeconds(2);
-            Destroy(collision.gameObject);  
+            Destroy(collision.gameObject); 
+            
             prop_clone_GO = (GameObject)Instantiate(Prop, new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z - 1), Quaternion.identity);
 
             yield return new WaitForSeconds(4);
+
             Destroy(portal);
+            Destroy(portal1);
+
             Destroy(gameObject);    
         }
     }
